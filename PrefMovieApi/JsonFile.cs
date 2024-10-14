ec558f2adb6edda605ad39c56c5c66fc4878b128
Kusieq <konrad.kus@edu.uekat.pl>
2024-10-14 14:19:17 +0200

    Creating class to load favorite movies and save.

---
+++ PrefMovieApi/JsonFile.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrefMovieApi
+{
+    public class JsonFile
+    {
+        /// <summary>
+        /// Serialize list to file
+        /// </summary>
+        /// <param name="listOfMoviesAndTvShows"> List of movies and tv shows</param>
+        public void SerializeLibrary(List<string> listOfMoviesAndTvShows)
+        {
+            MainWindow.logger.Log(LogLevel.Info, "SerializeLibrary Activated");
+            if(!(listOfMoviesAndTvShows.Count == 0 || listOfMoviesAndTvShows is null))
+            {
+                MainWindow.logger.Log(LogLevel.Info, "Library is NOT empty");
+                string jsonSerialize = JsonConvert.SerializeObject(listOfMoviesAndTvShows);
+                File.WriteAllText("LibraryFile.txt", jsonSerialize);
+            }
+            else
+            {
+                MainWindow.logger.Log(LogLevel.Warn, "Library is empty");
+            }
+        }
+
+        /// <summary>
+        /// Deserialize file into list
+        /// </summary>
+        /// <returns>List of strings</returns>
+        public List<string> DeserializeLibrary()
+        {
+            MainWindow.logger.Log(LogLevel.Info, "DeserializeLibrary Activated");
+            if (File.Exists("LibraryFile.txt"))
+            {
+                MainWindow.logger.Log(LogLevel.Info, "Reading file");
+                string readerFile = File.ReadAllText("LibraryFile.txt");
+                return JsonConvert.DeserializeObject<List<string>>(readerFile);
+            }
+            else
+            {
+                MainWindow.logger.Log(LogLevel.Warn,"File doesn't exist");
+                return new List<string>();
+            }
+        }
+    }
+}
--- PrefMovieApi/Library.xaml.cs
+++ PrefMovieApi/Library.xaml.cs
@@ -20,9 +20,33 @@ namespace PrefMovieApi
     /// </summary>
     public partial class Library : UserControl
     {
+        public static List<string> titles = new List<string>();
+        public static JsonFile jsonFile = new JsonFile();
         public Library()
         {
             InitializeComponent();
+
+            titles = jsonFile.DeserializeLibrary();
+            if(titles.Count > 0 )
+            {
+                MainWindow.logger.Log(LogLevel.Info, "In file is more than 0 titles");
+                SettingStackPanelLibrary();
+            }
+        }
+        public void SettingStackPanelLibrary()
+        {
+            MainWindow.logger.Log(LogLevel.Info, "Setting Library of items activated");
+            foreach( var item in titles )
+            {
+                // Creating item for stackpanel
+                TextBlock title = new TextBlock()
+                {
+                    Text = item,
+                };
+
+                // Adding item to stackpanel
+                FavoriteMovies.Children.Add(title);
+            }
         }
     }
 }
